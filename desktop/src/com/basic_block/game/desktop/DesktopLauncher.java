7cca8bd46a28a81768c194a762380d177901b632
liukwarm <liukwarm@gmail.com>
2015-08-26 22:43:37 -0700

    gradle build

---
+++ desktop/src/com/basic_block/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.basic_block.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.basic_block.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
